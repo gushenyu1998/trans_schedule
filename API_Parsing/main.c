b97677402449307bd07af07a358ce29d0f0e57eb
Shenyu Gu <shenyug@sfu.ca>
2024-03-25 16:34:48 -0700

    Working on parsing

--- API_Parsing/main.c
+++ API_Parsing/main.c
@@ -6,13 +6,19 @@
 #include <stdbool.h>
 
 struct json_object *parsed_json;
-struct json_object *first_route;
+struct json_object *route;
 struct json_object *route_no;
 struct json_object *route_name;
 struct json_object *direction;
 struct json_object *schedules;
-struct json_object *first_schedule;
+struct json_object *route_schedule;
+
 struct json_object *expected_leave_time;
+struct json_object *expected_count_down;
+struct json_object *destination;
+struct json_object *cancelled_trip;
+struct json_object *cancelled_Stop;
+
 
 struct MemoryStruct
 {
@@ -58,19 +64,17 @@ struct transStruct* ReadFromTransAPI(int *size, char * APIquery){
     CURL *curl;
     CURLcode res;
     struct MemoryStruct chunk;
-    chunk.memory = malloc(1);
+    chunk.memory = NULL;
     chunk.size = 0;
     curl_global_init(CURL_GLOBAL_ALL);
     curl = curl_easy_init();
     if (curl)
     {   
-        //Api Calling for example: "https://api.translink.ca/rttiapi/v1/stops/60980/estimates?apikey=JoKWW8MHpsoc04lKVKnA&count=5"
         curl_easy_setopt(curl, CURLOPT_URL, APIquery);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
         curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curl_slist_append(NULL, "Accept: application/JSON"));
-
         res = curl_easy_perform(curl);
         if (res != CURLE_OK)
         {
@@ -78,6 +82,7 @@ struct transStruct* ReadFromTransAPI(int *size, char * APIquery){
         }
         else
         {
+            transStruct *busSchedule;
             printf("%lu bytes retrieved\n", (unsigned long)chunk.size);
             parsed_json = json_tokener_parse(chunk.memory);
             size_t json_size = 0;
@@ -86,27 +91,33 @@ struct transStruct* ReadFromTransAPI(int *size, char * APIquery){
                 // Get the size of the top-level JSON array
                 json_size = json_object_array_length(parsed_json);
                 *size  = json_size;
+                busSchedule = malloc(sizeof(transStruct)*json_size);
             }
             else
             {
                 printf("Error: Top-level JSON is not an array.\n");
             }
-            // printf("JSON Size: %zu\n", json_size);
-            // for (size_t i = 0; i < json_size; i++)
-            // {
-            //     first_route = json_object_array_get_idx(parsed_json, i);
-            //     json_object_object_get_ex(first_route, "RouteNo", &route_no);
-            //     json_object_object_get_ex(first_route, "RouteName", &route_name);
-            //     json_object_object_get_ex(first_route, "Direction", &direction);
-            //     json_object_object_get_ex(first_route, "Schedules", &schedules);
-            //     first_schedule = json_object_array_get_idx(schedules, 0);
-            //     json_object_object_get_ex(first_schedule, "ExpectedLeaveTime", &expected_leave_time);
-            //     printf("Route:\n");
-            //     printf("Route No: %s\n", json_object_get_string(route_no));
-            //     printf("Route Name: %s\n", json_object_get_string(route_name));
-            //     printf("Direction: %s\n", json_object_get_string(direction));
-            //     printf("First Schedule's Expected Leave Time: %s\n", json_object_get_string(expected_leave_time));
-            // }
+            printf("JSON Size: %zu\n", json_size);
+            for (size_t i = 0; i < json_size; i++)
+            {
+                route = json_object_array_get_idx(parsed_json, i);
+                json_object_object_get_ex(route, "RouteNo", &route_no);
+                json_object_object_get_ex(route, "RouteName", &route_name);
+                json_object_object_get_ex(route, "Direction", &direction);
+                json_object_object_get_ex(route, "Schedules", &schedules);
+                busSchedule[i].RouteNo = json_object_get_string(route_no);
+                busSchedule[i].RouteName = json_object_get_string(route_name);
+                
+                
+                route_schedule = json_object_array_get_idx(schedules, 0);
+                size_t schedule_size = json_object_array_length(route_schedule);
+                json_object_object_get_ex(route_schedule, "ExpectedLeaveTime", &expected_leave_time);
+                printf("Route:\n");
+                printf("Route No: %s\n", json_object_get_string(route_no));
+                printf("Route Name: %s\n", json_object_get_string(route_name));
+                printf("Direction: %s\n", json_object_get_string(direction));
+                printf("First Schedule's Expected Leave Time: %s\n", json_object_get_string(expected_leave_time));
+            }
         }
         curl_easy_cleanup(curl);
         free(chunk.memory);
@@ -117,5 +128,9 @@ struct transStruct* ReadFromTransAPI(int *size, char * APIquery){
 
 int main(void)
 {
+    int size;
+    char *calling = "https://api.translink.ca/rttiapi/v1/stops/60980/estimates?apikey=JoKWW8MHpsoc04lKVKnA&count=2";
+    int count = 2;
+    ReadFromTransAPI(&size, calling);
     return 1;
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
-sudo dpkg --add-architecture armhf
-sudo apt-get install libjson-c-dev:armhf
-sudo apt-get install libcurl4-openssl-dev:armhf
+Compile the json parse
+1. sudo dpkg --add-architecture armhf
+2. sudo apt-get install libjson-c-dev
+2. sudo apt-get install libjson-c-dev:armhf
+3. sudo apt-get install libcurl4-openssl-dev:armhf
