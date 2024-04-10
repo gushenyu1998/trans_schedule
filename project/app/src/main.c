7a8f31eb4380b6ed81ccd128baeaed62edb87ff7
Jacky Lim <jackydroid68@gmail.com>
2024-04-10 14:06:18 -0700

    Update main.c

--- project/app/src/main.c
+++ project/app/src/main.c
@@ -4,6 +4,16 @@
 #include <flite/flite.h>
 #include <alsa/asoundlib.h>
 
+
+#include <stdlib.h>
+
+void speak_and_play(const char* text) {
+    char command[256];
+    snprintf(command, sizeof(command), "espeak '%s' -w test.wav > /dev/null 2>&1", text);
+    system(command);
+    system("aplay test.wav > /dev/null 2>&1");
+}
+
 int main(void)
 {
 //	int size;
@@ -11,14 +21,28 @@ int main(void)
 //	transStruct_t *a = (transStruct_t *) ReadFromTransAPI(&size, calling);
 //	freeTransStruct(size, a);
 
+    // ---------------------------- TTS ----------------------------
+
+//    espeak_Initialize(AUDIO_OUTPUT_PLAYBACK, 0, NULL, 0);
+//    espeak_SetVoiceByName("en-us+f4");
+//    espeak_SetParameter(espeakRATE, 170, 0);
+//    espeak_SetSynthCallback(SynthCallback);
+//    const char *text = "Bus fifty nine departing soon at stop one";
+//    unsigned int unique_identifier;
+//    espeak_Synth(text, strlen(text) + 1, 0, POS_CHARACTER, 0, espeakCHARS_AUTO, &unique_identifier, NULL);
+//    espeak_Synchronize();
 
     // ---------------------------- TTS ----------------------------
 
-    initialize_flite();
-    cst_voice *voice = setup_voice();
+//    initialize_flite();
+//    cst_voice *voice = setup_voice();
+//
+//    const char *text = "Bus fifty nine departing soon at stop one";
+//    synthesize_text_to_file(text, voice, "play");
 
-    const char *text = "Bus fifty nine departing soon at stop one";
-    synthesize_text_to_file(text, voice, "play");
+    // play the text with espeak
+    speak_and_play("Bus fifty nine departing soon at stop one");
+
 
     // ---------------------------- NFC ----------------------------
 
